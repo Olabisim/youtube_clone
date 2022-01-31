d2c684ac7f4c5c09f65876e3b57511ec53a65234
holabisii <ajoseholabisi@gmail.com>
2022-01-31 06:41:01 -0500

    youtube api to fetch videos and env added

--- Search.js
+++ Search.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, FlatList, ActivityIndicator as AI } from 'react-native';
 import { AntDesign, Feather } from '@expo/vector-icons';
 import MiniCard from '../components/MiniCard';
-
+import { YOUTUBE_API_KEY } from 'react-native-dotenv'
 
 
 const SearchScreen = () => {
 
 
         const [value, setValue] = useState('')
+        const [miniCardData, SetMiniCardData] = useState([])
+        const [loading, setLoading] = useState(false)
+
+        const fetchData = () => {
+
+                setLoading(true)
+                fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${value}&type=video&key=${YOUTUBE_API_KEY}`)
+                .then ( res => res.json())
+                .then ( data => {
+                        // console.log(data)
+                        setLoading(false)
+                        SetMiniCardData(data.items)
+                })
+                .catch(err => console.log(err))
+        }
+
 
         return (
                 <View style={{flex: 1}}>
@@ -23,9 +39,31 @@ const SearchScreen = () => {
                                         onChangeText={(text) => setValue(text)}
                                 />
 
-                                <Feather name="send" size={24} color="black" />
+                                <Feather name="send" size={24} color="black" 
+                                        onPress={() => fetchData()}
+                                />
 
-                                <MiniCard />
+                                {
+                                        loading 
+                                        &&
+                                        <AI size="large" color="red" style={{marginTop: 10}} />
+
+                                }
+
+                                <FlatList 
+                                
+                                        data = {miniCardData}
+                                        renderItem={item => {
+                                                return <MiniCard 
+                                                                videoId={item.id.videoId} 
+                                                                title={item.snippet.title}
+                                                                channel={item.snippet.channelTitle}
+                                                        />
+                                        }}
+                                        keyExtractor={item => item.id.videoId}
+
+                                />
+
                         </View>
                 
                 </View>
--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { StyleSheet, T as T, V as V, I as I, D as D} from 'react-native';
 
 
-const MiniCard = () => {
+// image :::  https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60
+const MiniCard = ({ title, videoId, channel }) => {
 
         return (
                 <V style={{flexDirection: "row", margin: 12, marginBottom: 0}}>
                         
                         <I 
-                                source={{uri: "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw1MHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60"}}
+                                source={{uri: `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`}}
                                 style={{ 
                                         width: "45%",
                                         height: 100
@@ -25,9 +26,9 @@ const MiniCard = () => {
                                         ellipsizeMode= "tail"
                                         numberOfLines={3}
                                 >
-                                        This is an amazing course
+                                        {title}
                                 </T>
-                                <T style={{fontSize: 10}}>coders never quit</T>
+                                <T style={{fontSize: 10}}>{channel}</T>
 
                         </V>
 
